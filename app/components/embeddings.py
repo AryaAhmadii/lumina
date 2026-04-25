c1d49c18d10d81e57dccaf86e075973b0e00a8cd
Arya Ahmadi <107758775+AryaAhmadii@users.noreply.github.com>
2026-04-26 00:53:32 +0330

    Update embeddings.py

--- app/components/embeddings.py
+++ app/components/embeddings.py
@@ -1,1 +1,11 @@
+from langchain_huggingface import HuggingFaceEmbeddings
 
+from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+
+logger = get_logger(__name__)
+
+def get_embedding_model():
+  try:
+    logger.info("initializing huggingface embedding model")
+    
