df9eba083ee2f6615eeee1a6b8b68692ea341570
Arya Ahmadi <107758775+AryaAhmadii@users.noreply.github.com>
2026-04-25 17:25:35 +0330

    Update config.py

--- app/configs/config.py
+++ app/configs/config.py
@@ -6,3 +6,13 @@ CHUNK_SIZE = os.environ.get("CHUNK_SIZE")
 CHUNK_OVERLAP = os.environ.get("CHUNK_OVERLAP")
 FAISS_PATH = os.environ.get("DB_FAISS_PATH")
 HF_REPO_ID = os.environ.get("HF_REPO_ID")
+
+def cnf:
+  return {
+    "HF_TOKEN": HF_TOKEN,
+    "DATA_PATH": DATA_PATH,
+    "CHUNK_SIZE": CHUNK_SIZE,
+    "CHUNK_OVERLAP": CHUNK_OVERLAP,
+    "FAISS_PATH": FAISS_PATH,
+    "HF_REPO_ID": HF_REPO_ID
+  }
