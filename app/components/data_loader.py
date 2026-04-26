fcf4657cb2f7ab9d28171426182a960c86d028b3
Arya Ahmadi <107758775+AryaAhmadii@users.noreply.github.com>
2026-04-26 17:32:53 +0330

    Create data_loader.py

---
+++ app/components/data_loader.py
@@ -0,0 +1,22 @@
+import os
+
+from app.components.pdf_loader import load_books, create_text_chunks
+from app.components.vector_store import save_vector_store
+from app.configs.config import cnf
+from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+
+config = cnf()
+logger = get_logger(__name__)
+
+def process_store_pdfs():
+  try:
+    logger.info("creating vectorstore")
+
+    docs = load_books()
+    chunks = create_text_chunks(docs)
+    save_vector_store(chunks)
+
+  except Exception as e:
+    logger.error(str(CustomException("failed to create vectorstore", e)))
+
