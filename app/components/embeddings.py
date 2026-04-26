2f0825f8c43bf2fb9ca2b94b2c0caad8679a85f3
Arya Ahmadi <107758775+AryaAhmadii@users.noreply.github.com>
2026-04-26 10:19:43 +0330

    Update embeddings.py

--- app/components/embeddings.py
+++ app/components/embeddings.py
@@ -8,4 +8,13 @@ logger = get_logger(__name__)
 def get_embedding_model():
   try:
     logger.info("initializing huggingface embedding model")
-    
+
+    model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+    logger.info("huggingface embedding model loaded successfully")
+
+    return model
+
+  except Exception as e:
+    logger.error(str(CustomException("can not load embedding model", e)))
+    return
