ed9570def5c2409d096c369f5eb71106f5cdfb21
Arya Ahmadi <107758775+AryaAhmadii@users.noreply.github.com>
2026-04-28 23:48:59 +0330

    base structure of main app

--- app/app.py
+++ app/app.py
@@ -1,1 +1,12 @@
+import os
 from flask import Flask, render_template, request, session, redirect, url_for
+
+from app.components.retriever import create_chain
+from dotenv import load_dotenv
+
+load_dotenv()
+
+hf_token = os.environ.get("HF_TOKEN")
+
+app = Flask(__name__)
+app.secret_key = os.urandom()
