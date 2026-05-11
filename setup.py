a5d671e2288418bda19378f53deadb869f448563
dwrrio <107758775+dwrrio@users.noreply.github.com>
2026-05-11 21:18:46 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 setup(
     name="lumina",
     version="1",
-    author="ARYA",
+    author="dwrrio",
     packages=find_packages(),
     install_requires = requirements,
 )
